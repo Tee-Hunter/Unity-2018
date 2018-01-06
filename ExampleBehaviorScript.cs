7921561c119ad207460ee9728e16f60b71d8e3f0
TEE-HUNTER <theechate@gmail.com>
2018-01-06 21:01:32 +0700

    UnitOne

---
+++ ExampleBehaviorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleBehaviorScript : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
