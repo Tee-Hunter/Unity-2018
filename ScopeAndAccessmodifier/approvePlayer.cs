c1983af3cf6e86fb7309138c99f7835ba5ee8518
TEE-HUNTER <theechate@gmail.com>
2018-01-11 22:54:58 +0700

    Add Unit 07 Scope and Access Modifier

---
+++ ScopeAndAccessmodifier/approvePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class approvePlayer  {
+
+    // ປຶນສອງກະບອກນີ້ player ໃນທີມສາມາດໃຊ້ທຸກຄົນ (public scope)
+    public int GSF302; // ປືນ GSF302
+    public int GSF506; // ປືນ GSF506
+
+    // ປຶນສອງກະບອກນີ້ໃຊ້ໄດ້ສະເພາະລູກທີມບາງຄົນທີ່ເປັນ ApprovePlayer ເທົ່ານັນ (local scope)
+    private int H2C01; // ປືນ H2C01
+    private int H7C09; // ປືນ H7C09
+
+    // method for first weapon
+    public void weapon0 (int GSF302, int GSF506) // ຮັບປືນຈາກ public scope (ປືນທີ່ຖືກກົດໝາຍສາມາດຫາໃຊ້ໄດ້ທົ່ວໄປ)
+    {
+        int shotOut;
+        
+        shotOut = GSF302 + GSF506;
+        Debug.Log("Total shot : " + shotOut);
+        
+    }
+
+    // method for second weapon
+    private void weapon1 (int H2C01, int H7C09) // ຮັບປືນຈາກ private scope (ປືນທີ່ຜິດກົດໝາຍຕ້ອງລັກຊື້ກັນແບບລັບໆ)
+    {
+        int shotOut;
+
+        shotOut = H2C01 + H7C09;
+        Debug.Log("Total shot of weapon1 : " + shotOut);
+    }
+}
---
+++ ScopeAndAccessmodifier/mainPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class ຫຼັກ 
+public class mainPlayer : MonoBehaviour {
+
+    public int item = 5;    // item ຂອງ player
+
+    private int unemy = 0;  // team member 
+    private int level = 5;  // level of player 
+
+    public approvePlayer shooter; // ປະກາດ Class approvePlayer
+
+	void Start () {
+
+        item = 30;  // ເລີ່ມເກມດ້ອຍ item = 30
+
+        // 
+        approvePlayer shooter = new approvePlayer();    // ເອີ້ນໃຊ້ Class approvePlayer
+        shooter.weapon0 (item, shooter.GSF302);
+
+	}
+	
+    // ສ້າງ method ໃຫ້ກັບຫົວໜ້າທີມ ສາມາດຍິງໄດ້ຫຼາຍກວ່າລຸກທີມ 
+    void maintShot (int oneShot, int twiceShot) // ຮັບຄ່າຈາກ unemy ແລະ level
+    {
+        int shootOut; 
+
+        shootOut = oneShot * twiceShot * item;
+        Debug.Log(shootOut);
+    }
+
+	// update item of the main player ວົນລູບຕະຫຼອດເວລາເປີດເກມ ໄວຊ້າຂື້ນກັບ FPS ຂອງຄອມແຕ່ລະໜ່ວຍ
+	void Update () {
+        Debug.Log("Your item now is " + item);
+	}
+}
