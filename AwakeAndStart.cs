e103ddb614bb7f9f89452085f8e9e1fa0c47d677
TEE-HUNTER <theechate@gmail.com>
2018-01-15 22:55:54 +0700

    Add Unit 08

---
+++ AwakeAndStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour {
+
+    // ຈະລັນທັນທີ່ເມື່ອ code ເລີ່ມທຳງານ ບໍ່ຈຳເປັນຕ້ອງເອີ້ນໃຊ້ GameObject ກໍສາມາໃຊ້ງານໄດ້
+    void Awake()
+    {
+        Debug.Log("The Awake Fuction");
+    } 
+
+	// ຄ້າຍຄືກັບ Awake ແຕ່ຈະທຳງານກໍຕໍ່ເມື່ອ GameObject ຖືກເອີ້ນໃຊ້
+	void Start ()
+    {
+        Debug.Log("The Start Fuction");
+	}
+	
+	
+}
