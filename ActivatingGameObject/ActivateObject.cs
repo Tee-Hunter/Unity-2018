f4e0c127e43b271272a4b87790dc2ce48ba38a98
TEE-HUNTER <theechate@gmail.com>
2018-01-18 13:57:33 +0700

    Add Unit 12

---
+++ ActivatingGameObject/ActivateObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour {
+
+	void Start()
+    {
+        // ກຳນົດປຶດ GameObject 
+        gameObject.SetActive(false);    // false ປິດ ແລະ true ເປີດ
+    }
+}
---
+++ ActivatingGameObject/CheckStates.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckStates : MonoBehaviour {
+
+    //ເອື້ນໃຊ້ Activate Object ຜ່ານຕົວປ່ຽນ myGameObject
+    public GameObject myGameObject;
+	
+	void Start ()
+    {
+        Debug.Log("Active seft : " + myGameObject.activeSelf);
+        Debug.Log("Active in Hierarchy : " + myGameObject.activeInHierarchy);
+	}
+	
+}
